0ff886aad7719d8fda9345f5f878196bcd71c5ce
UglanovEA <uglanovea@gmail.com>
2020-10-06 13:16:13 +0300

    ver 3.7

--- code/code.js
+++ code/code.js
@@ -4,3 +4,45 @@
 // alert(admin);
 // let ourPlanetName = "Земля"
 // let currentUserName = "Пользователь"
+// let name = prompt("Привет, как тебя зовут");
+// alert(`Меня зовут ${name}`);
+// let isBoss = confirm("Ты здесь главный?");
+// alert(isBoss);
+// let yourName = prompt("Как тебя зовут?");
+// alert(`Меня зовут ${yourName}`);
+
+// let a = 1, b = 1;
+// let c = ++a; // 2
+// let d = b++; // 1
+
+// let a = 2;
+// let x = 1 + (a *= 2); // a=4 x =5
+
+// "" + 1 + 0 // 10 сложение со строкой преобразует в строку
+// "" - 1 + 0 // -1 вычитание превращает строку в ноль
+// true + false // 1
+// 6 / "3" // 2
+// "2" * "3" // 6
+// 4 + 5 + "px" //9px
+// "$" + 4 + 5 //$45
+// "4" - 2 // 2
+// "4px" - 2 //nan
+// 7 / 0 //infinity
+// "  -9  " + 5 //" -9 5"
+// "  -9  " - 5 // -14
+// null + 1 //1 null это ноль
+// undefined + 1 //nan undefined становиться nan
+// " \t \n" - 2  //-2
+
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+
+// alert(+a + +b); // 12
+
+// 5 > 4 // true
+// "ананас" > "яблоко" // false
+// "2" > "12" // true
+// undefined == null // true
+// undefined === null //false
+// null == "\n0\n" // false (null равен только undefined)
+// null === +"\n0\n" // false (Строгое сравнение разных типов.)

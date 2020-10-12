c07a93e0fff5cb5d11dc569441c8b14f856c9463
UglanovEA <uglanovea@gmail.com>
2020-10-12 15:10:06 +0300

    ver 4.2

--- open-close/open-close.js
+++ open-close/open-close.js
@@ -1,14 +1,20 @@
+// Находим элементы на странице
 const btn = document.querySelector("#button");
 const content = document.querySelector("#content");
 
+// По нажатию на кнопку, запускаем функцию проверки класса
 btn.addEventListener("click", btnClick);
 
+
 function btnClick() {
+  // Делаем проверку класса методом contains
   if (content.classList.contains("content-hidden")) {
+    // Прописывает необходимый текст
     btn.textContent = "Закрыть блок";
   } else {
     btn.textContent = "Открыть блок";
   }
 
+  // Переключаем класс
   content.classList.toggle("content-hidden");
 }

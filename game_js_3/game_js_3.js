6264808edaeb610c6bce46aeb7662e01009823b1
UglanovEA <uglanovea@gmail.com>
2021-01-21 16:44:16 +0300

    ver 5.5

--- game_js_3/game_js_3.js
+++ game_js_3/game_js_3.js
@@ -7,43 +7,64 @@ const mapHeight = canvas.offsetHeight;
 const world = {
   width: canvas.offsetWidth,
   height: canvas.offsetHeight,
-  speed: 1,
+  speed: 0.6,
   draw() {
     ctx.fillStyle = '#add8e6'
     ctx.fillRect(0, 0, mapWidth, mapHeight)
   }
 }
 
-const tube = {
-  x: 300,
-  y: 0,
-  width: 20,
-  height: 70,
-  distance: 40,
-  draw() {
+function Tube(x) {
+  this.x = x
+  this.y = 0
+  this.width = 20
+  this.height = 70
+  this.distance = 40
+  this.draw = () => {
     ctx.fillStyle = '#008000'
-    ctx.fillRect(tube.x, tube.y, tube.width, tube.height)
-    ctx.fillRect(tube.x, tube.height + tube.distance, tube.width, world.height)
+    ctx.fillRect(this.x, this.y, this.width, this.height)
+    ctx.fillRect(this.x, this.height + this.distance, this.width, world.height)
   }
 }
 
 const bird = {
   x: 20,
-  y: 90,
-  width: 17,
-  height: 16,
+  y: 20,
+  width: 14,
+  height: 12,
+  gravity: 0,
   img: new Image(this.width, this.height),
   draw() {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+  },
+  fly() {
+    this.gravity = this.gravity = -3
   }
 }
-bird.img.src = './img/bird.png'
 
+bird.img.src = './img/bird.png'
+window.onkeydown = () => bird.fly() // Движение по нажатию клавиши
+
+const tubes = [new Tube(world.width), new Tube(world.width + world.width / 2)]
+function reset() {
+  bird.y = 20
+  bird.gravity = 1
+}
 function render() {
   world.draw()
-  tube.draw()
   bird.draw()
 
-  tube.x = tube.x - world.speed
+  tubes.forEach(tube => {
+    tube.draw()
+     tube.x = tube.x - world.speed
+  })
+  
+  bird.y = bird.y + bird.gravity
+  bird.gravity = bird.gravity + 0.3
+
+  const isBirdOutOfScreen = bird.y > world.height
+  if (isBirdOutOfScreen) {
+    reset()
+  }
 };
 setInterval(render, 1000 / 60)

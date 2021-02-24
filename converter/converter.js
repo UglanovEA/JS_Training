80bdcc94b72f77845e62f2e6090033f350ed8d36
UglanovEA <uglanovea@gmail.com>
2021-02-24 16:57:41 +0300

    ver 6.3

--- converter/converter.js
+++ converter/converter.js
@@ -9,9 +9,13 @@ const rates = {}
 const elementUSD = document.querySelector('[data-value="USD"]')
 const elementEUR = document.querySelector('[data-value="EUR"]')
 const elementGBP = document.querySelector('[data-value="GBP"]')
+const input = document.querySelector('#input')
+const result = document.querySelector('#result')
+const select = document.querySelector('#select')
 
 getCurrencies()
 
+// Получение курса валют и отображение
 async function getCurrencies() {
   const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
   const data = await response.json();
@@ -41,4 +45,12 @@ async function getCurrencies() {
     elementGBP.classList.add('bottom')
   }
 
+}
+
+input.oninput = convertValue;
+select.oninput = convertValue;
+
+// Функция конвертации
+function convertValue() {
+  result.value = (parseFloat(input.value) / rates[select.value].Value).toFixed(2)
 }

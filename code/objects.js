85fc4d8f0f4cfe10431ac71b1fa4a22f5fc822b8
UglanovEA <uglanovea@gmail.com>
2021-05-28 14:23:27 +0300

    ver 7.6

---
+++ code/objects.js
@@ -0,0 +1,32 @@
+/*
+var obj1 = new Object(),
+  obj2 = {},
+  obj3 = Object.create(null);
+*/
+/*=========================================================*/
+/*
+let dataUser = {
+  name: 'Robin'
+};
+function newUser(dataUser) {
+  dataUser = { name: 'Kate' };
+}
+newUser(dataUser);
+console.log(dataUser.name) // robin
+*/
+/*=========================================================*/
+/*
+// Какой вариант создания объекта не верный
+var obj = new Object();
+let obj = new Object{}; // ошибка syntaxerror: unexpected token
+let obj = {};
+let obj = new Object();
+*/
+/*=========================================================*/
+/*
+let obj = new Object();
+console.log(obj.a); //undefined
+obj.a = 5;
+console.log(obj.a) //5
+*/
+/*=========================================================*/

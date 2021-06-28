bc2cd3648ae8ff5611e24a35fd558b571684dc77
UglanovEA <uglanovea@gmail.com>
2021-06-28 17:48:15 +0300

    ver 8.7

---
+++ code/codeMu.js
@@ -0,0 +1,89 @@
+// alert(true + 3);
+// alert(true + true);
+// alert(true - true);
+// alert('1' + true);
+// alert(String(true) + 1);
+// alert(String(true) + Number(true));
+
+////////////////////////////////////////////////////
+
+// let test = Boolean(3);
+// alert(test);
+// let test = Boolean(0);
+// alert(test);
+// let test = Boolean(-1);
+// alert(test);
+// let test = Boolean(-0);
+// alert(test);
+// let test = Boolean(+0);
+// alert(test);
+// let test = Boolean('abc');
+// alert(test);
+// let test = Boolean('');
+// alert(test);
+// let test = Boolean('0');
+// alert(test);
+// let test = Boolean(true);
+// alert(test);
+// let test = Boolean(false);
+// alert(test);
+// let test = Boolean('true');
+// alert(test);
+// let test = Boolean('false');
+// alert(test);
+// let test = Boolean(null);
+// alert(test);
+// let test = Boolean('null');
+// alert(test);let test = Boolean(undefined);
+// let test = Boolean(undefined);
+// alert(test);
+// let test = Boolean('undefined');
+// alert(test);
+// let test = Boolean(NaN);
+// alert(test);
+// let test = Boolean('NaN');
+// alert(test);
+// let test = Boolean(3 * 'abc');
+// alert(test);
+// let test = Boolean(Infinity);
+// alert(test);
+// let test = Boolean(1 / 0);
+// alert(test);
+
+////////////////////////////////////////////////////
+
+// let str = 'abcde'
+// console.log(str[0])
+// console.log(str[2])
+// console.log(str[4])
+// let str = 'abcde'
+// let newStr = str[4] + str[3] + str[2] + str[1] + str[0]
+// console.log(newStr)
+// let str = 'abcde'
+// let num = 3
+// console.log(str[num])
+// let str = 'abcde';
+// let last = str.length - 1;
+// console.log(last)
+// console.log(str[last])
+// let str = 'abcde';
+// let last = str.length - 2;
+// console.log(last)
+// console.log(str[last])
+// let str = 'abcde';
+// let last = str.length - 3;
+// console.log(last)
+// console.log(str[last])
+// let test = '12345';
+// alert(Number(test[0]) + Number(test[1]) + Number(test[2]) + Number(test[3]) + Number(test[4]));
+// let test = 12345;
+// let str = String(test)
+// console.log(Number(str[0]) + Number(str[1]) + Number(str[2]) + Number(str[3]) + Number(str[4]));
+// let test = 12345;
+// let str = String(test)
+// console.log((str[0]) * (str[1]) * (str[2]) * (str[3]) * (str[4]));
+// let test = 12345;
+// let str = String(test)
+// let newStr = str[4] + str[3] + str[2] + str[1] + str[0]
+// test = Number(newStr)
+// console.log(test)

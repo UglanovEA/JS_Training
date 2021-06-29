27090e2ed4fae5d1bb13a953d8960de96b58633c
UglanovEA <uglanovea@gmail.com>
2021-06-29 17:54:49 +0300

    ver 9.1

--- code/codeMu.js
+++ code/codeMu.js
@@ -1,54 +1,54 @@
-// alert(true + 3);
-// alert(true + true);
-// alert(true - true);
-// alert('1' + true);
-// alert(String(true) + 1);
-// alert(String(true) + Number(true));
+// alert(true + 3)
+// alert(true + true)
+// alert(true - true)
+// alert('1' + true)
+// alert(String(true) + 1)
+// alert(String(true) + Number(true))
 
 ////////////////////////////////////////////////////
 
-// let test = Boolean(3);
-// alert(test);
-// let test = Boolean(0);
-// alert(test);
-// let test = Boolean(-1);
-// alert(test);
-// let test = Boolean(-0);
-// alert(test);
-// let test = Boolean(+0);
-// alert(test);
-// let test = Boolean('abc');
-// alert(test);
-// let test = Boolean('');
-// alert(test);
-// let test = Boolean('0');
-// alert(test);
-// let test = Boolean(true);
-// alert(test);
-// let test = Boolean(false);
-// alert(test);
-// let test = Boolean('true');
-// alert(test);
-// let test = Boolean('false');
-// alert(test);
-// let test = Boolean(null);
-// alert(test);
-// let test = Boolean('null');
-// alert(test);let test = Boolean(undefined);
-// let test = Boolean(undefined);
-// alert(test);
-// let test = Boolean('undefined');
-// alert(test);
-// let test = Boolean(NaN);
-// alert(test);
-// let test = Boolean('NaN');
-// alert(test);
-// let test = Boolean(3 * 'abc');
-// alert(test);
-// let test = Boolean(Infinity);
-// alert(test);
-// let test = Boolean(1 / 0);
-// alert(test);
+// let test = Boolean(3)
+// alert(test)
+// let test = Boolean(0)
+// alert(test)
+// let test = Boolean(-1)
+// alert(test)
+// let test = Boolean(-0)
+// alert(test)
+// let test = Boolean(+0)
+// alert(test)
+// let test = Boolean('abc')
+// alert(test)
+// let test = Boolean('')
+// alert(test)
+// let test = Boolean('0')
+// alert(test)
+// let test = Boolean(true)
+// alert(test)
+// let test = Boolean(false)
+// alert(test)
+// let test = Boolean('true')
+// alert(test)
+// let test = Boolean('false')
+// alert(test)
+// let test = Boolean(null)
+// alert(test)
+// let test = Boolean('null')
+// alert(test);let test = Boolean(undefined)
+// let test = Boolean(undefined)
+// alert(test)
+// let test = Boolean('undefined')
+// alert(test)
+// let test = Boolean(NaN)
+// alert(test)
+// let test = Boolean('NaN')
+// alert(test)
+// let test = Boolean(3 * 'abc')
+// alert(test)
+// let test = Boolean(Infinity)
+// alert(test)
+// let test = Boolean(1 / 0)
+// alert(test)
 
 ////////////////////////////////////////////////////
 
@@ -62,27 +62,27 @@
 // let str = 'abcde'
 // let num = 3
 // console.log(str[num])
-// let str = 'abcde';
+// let str = 'abcde'
 // let last = str.length - 1;
 // console.log(last)
 // console.log(str[last])
-// let str = 'abcde';
+// let str = 'abcde'
 // let last = str.length - 2;
 // console.log(last)
 // console.log(str[last])
-// let str = 'abcde';
+// let str = 'abcde'
 // let last = str.length - 3;
 // console.log(last)
 // console.log(str[last])
-// let test = '12345';
-// alert(Number(test[0]) + Number(test[1]) + Number(test[2]) + Number(test[3]) + Number(test[4]));
-// let test = 12345;
+// let test = '12345'
+// alert(Number(test[0]) + Number(test[1]) + Number(test[2]) + Number(test[3]) + Number(test[4]))
+// let test = 12345
 // let str = String(test)
-// console.log(Number(str[0]) + Number(str[1]) + Number(str[2]) + Number(str[3]) + Number(str[4]));
-// let test = 12345;
+// console.log(Number(str[0]) + Number(str[1]) + Number(str[2]) + Number(str[3]) + Number(str[4]))
+// let test = 12345
 // let str = String(test)
-// console.log((str[0]) * (str[1]) * (str[2]) * (str[3]) * (str[4]));
-// let test = 12345;
+// console.log((str[0]) * (str[1]) * (str[2]) * (str[3]) * (str[4]))
+// let test = 12345
 // let str = String(test)
 // let newStr = str[4] + str[3] + str[2] + str[1] + str[0]
 // test = Number(newStr)
@@ -90,67 +90,113 @@
 
 ////////////////////////////////////////////////////
 
-// let num = 1;
-// num = num + 1;
-// num = num + 1;
-// alert(num);
-// let num = 1;
-// num = num + 2;
-// num = num + 3;
-// alert(num);
-// let num = 47;
-// num += 7;
-// num -= 18;
-// num *= 10;
-// num /= 15;
-// alert(num);
+// let num = 1
+// num = num + 1
+// num = num + 1
+// alert(num)
+// let num = 1
+// num = num + 2
+// num = num + 3
+// alert(num)
+// let num = 47
+// num += 7
+// num -= 18
+// num *= 10
+// num /= 15
+// alert(num)
 
 ////////////////////////////////////////////////////
 
-// let num = 10;
-// num++;
-// num++;
-// num--;
-// alert(num);
-// let num = 3;
-// alert(++num);
-// let num = 3;
-// alert(num++);
-// let num = 3;
-// alert(--num);
-// let num = 3;
-// alert(num--);
-// let num1 = 3;
-// let num2 = ++num1;
-// alert(num1);
-// alert(num2);
-// let num1 = 3;
-// let num2 = num1++;
-// alert(num1);
-// alert(num2);
-// let num1 = 3;
-// let num2 = --num1;
-// alert(num1);
-// alert(num2);
-// let num1 = 3;
-// let num2 = num1--;
-// alert(num1);
-// alert(num2);
-// let num1 = 3;
-// num1++;
-// let num2 = num1--;
-// alert(num1++);
-// alert(--num2);
+// let num = 10
+// num++
+// num++
+// num--
+// alert(num)
+// let num = 3
+// alert(++num)
+// let num = 3
+// alert(num++)
+// let num = 3
+// alert(--num)
+// let num = 3
+// alert(num--)
+// let num1 = 3
+// let num2 = ++num1
+// alert(num1)
+// alert(num2)
+// let num1 = 3
+// let num2 = num1++
+// alert(num1)
+// alert(num2)
+// let num1 = 3
+// let num2 = --num1
+// alert(num1)
+// alert(num2)
+// let num1 = 3
+// let num2 = num1--
+// alert(num1)
+// alert(num2)
+// let num1 = 3
+// num1++
+// let num2 = num1--
+// alert(num1++)
+// alert(--num2)
 
 ////////////////////////////////////////////////////
 
 // alert('Ваш возраст: 22' + prompt('Ваш возраст?'))
-// let num1 = prompt('Введите первое число');
-// let num2 = prompt('Введите второе число');
-// alert(num1 + num2);
-// let num1 = prompt('Введите первое число');
-// let num2 = prompt('Введите второе число');
-// alert(Number(num1) + Number1(num2));
+// let num1 = prompt('Введите первое число')
+// let num2 = prompt('Введите второе число')
+// alert(num1 + num2)
+// let num1 = prompt('Введите первое число')
+// let num2 = prompt('Введите второе число')
+// alert(Number(num1) + Number1(num2))
+
+////////////////////////////////////////////////////
+
+// let arr = ['a', 'b', 'c']
+// alert(arr)
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[0] + '+' + arr[1] + '+' + arr[2])
+// let arr = [1, 2, 3]
+// console.log(arr[0] + arr[1] + arr[2])
+// let arr = [2, 5, 3, 9]
+// let arr1 = arr[0] * arr[1]
+// let arr2 = arr[2] * arr[3]
+// let result = arr1 + arr2
+// console.log(result)
+// let arr = ['a', 'b', 'c']
+// console.log(arr.length)
+// let arr = [1, 2, 3]
+// console.log(arr[arr.length - 1])
+// let arr = ['a', 'b', 'c']
+// arr[0] = 1
+// arr[1] = 2
+// arr[2] = 3
+// console.log(arr)
+// let arr = [1, 2, 3]
+// // arr[0] = arr[0] + 3
+// arr[1] = arr[1] + 3
+// arr[2] = arr[2] + 3
+// arr[0]++
+// arr[1]++
+// arr[2]++
+// console.log(arr)
+// let arr = []
+// arr[0] = 1
+// arr[1] = 2
+// arr[2] = 3
+// console.log(arr)
+// let arr = [1, 2, 3]
+// arr[3] = 4
+// arr[4] = 5
+// console.log(arr)
+// let arr = [1, 2, 3]
+// arr[10] = '!'
+// console.log(arr)
 
 ////////////////////////////////////////////////////
 

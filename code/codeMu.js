e7040d00a881960392d235a4b53181136284ddce
UglanovEA <uglanovea@gmail.com>
2021-06-30 17:27:48 +0300

    ver 9.3

--- code/codeMu.js
+++ code/codeMu.js
@@ -246,3 +246,27 @@
 
 ////////////////////////////////////////////////////
 
+// let arr = ['a', 'b', 'c']
+// let key = 2
+// console.log(arr[key])
+// let arr = [1, 2, 3, 4, 5]
+// let key1 = 1
+// let key2 = 2
+// console.log(arr[key1] + arr[key2])
+// let obj = {
+//   'a': 1,
+//   'b': 2,
+//   'c': 3
+// }
+// let key = 'a'
+// console.log(obj[key])
+// let text = 'string'
+// let obj = {
+//   text: '!!!',
+//   string: '???'
+// };
+// console.log(obj[text])
+
+////////////////////////////////////////////////////
+
+

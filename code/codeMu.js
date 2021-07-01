46e3a5efd4ef22503e06a9ddaed1233acfded086
UglanovEA <uglanovea@gmail.com>
2021-07-01 17:47:50 +0300

    ver 9.9

--- code/codeMu.js
+++ code/codeMu.js
@@ -374,3 +374,21 @@
 
 ////////////////////////////////////////////////////
 
+// let result = 1
+// for (let i = 1; i <= 20; i++) {
+//   result = result * i
+//   console.log(result)
+// }
+// let result = 0
+// for (let i = 0; i <= 100; i += 2) {
+//   result = result + i
+//   console.log(result)
+// }
+// let result = 0
+// for (let i = 1; i <= 100; i++) {
+//   result = result + i
+//   console.log(result)
+// }
+
+////////////////////////////////////////////////////
+

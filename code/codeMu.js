d2de0e536894025954993154a059fda99470cfeb
UglanovEA <uglanovea@gmail.com>
2021-07-06 16:43:59 +0300

    ver 11.2

--- code/codeMu.js
+++ code/codeMu.js
@@ -750,3 +750,25 @@
 //   arr[j] = i
 // }
 // console.log(arr)
+
+////////////////////////////////////////////////////
+
+// let arr = [1, 2, 3, 4, 5]
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i] = arr[i] * arr[i]
+// }
+// console.log(arr)
+// let arr = [1, 2, 3, 4, 5]
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i] -= 1
+// }
+// console.log(arr)
+// let arr = [1, 2, 3, 4, 5]
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i] += 10
+// }
+// console.log(arr)
+
+////////////////////////////////////////////////////
+
+

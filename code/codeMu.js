764b5e86e284d29c664aebf3e914e5d40777d561
UglanovEA <uglanovea@gmail.com>
2021-07-14 16:35:55 +0300

    ver 12.9

--- code/codeMu.js
+++ code/codeMu.js
@@ -1636,4 +1636,234 @@
 
 ////////////////////////////////////////////////////
 
+// let users = [
+//   {
+//     name: 'name1',
+//     age: 31,
+//   },
+//   {
+//     name: 'name2',
+//     age: 32,
+//   },
+//   {
+//     name: 'name3',
+//     age: 33,
+//   },
+// ]
+// for (let user of users) {
+//   document.write(user.name + ' ' + user.age + '<br>')
+// }
+// let employees = [
+//   {
+//     name: 'name1',
+//     salary: 300,
+//   },
+//   {
+//     name: 'name2',
+//     salary: 400,
+//   },
+//   {
+//     name: 'name3',
+//     salary: 500,
+//   },
+// ]
+// for (let user of employees) {
+//   document.write(user.name + '-' + user.salary + '<br>')
+// }
+// let employees = [
+//   {
+//     name: 'name1',
+//     salary: 300,
+//   },
+//   {
+//     name: 'name2',
+//     salary: 400,
+//   },
+//   {
+//     name: 'name3',
+//     salary: 500,
+//   },
+// ]
+// let sum = 0
+// for (let user of employees) {
+//   sum += user.salary
+// }
+// console.log(sum)
+// let employees = [
+//   {
+//     name: 'name1',
+//     salary: 300,
+//     age: 28,
+//   },
+//   {
+//     name: 'name2',
+//     salary: 400,
+//     age: 29,
+//   },
+//   {
+//     name: 'name3',
+//     salary: 500,
+//     age: 30,
+//   },
+//   {
+//     name: 'name4',
+//     salary: 600,
+//     age: 31,
+//   },
+//   {
+//     name: 'name5',
+//     salary: 700,
+//     age: 32,
+//   },
+// ]
+// let sum = 0
+// for (let user of employees) {
+//   if (user.age >= 30) {
+//     sum += user.salary
+//   }
+// }
+// console.log(sum)
+// let employees = [
+//   {
+//     name: 'name1',
+//     salary: 300,
+//     dismissed: false,
+//   },
+//   {
+//     name: 'name2',
+//     salary: 400,
+//     dismissed: true,
+//   },
+//   {
+//     name: 'name3',
+//     salary: 500,
+//     dismissed: false,
+//   },
+//   {
+//     name: 'name4',
+//     salary: 600,
+//     dismissed: true,
+//   },
+//   {
+//     name: 'name5',
+//     salary: 700,
+//     dismissed: false,
+//   },
+// ]
+// for (let user of employees) {
+//   if (user.dismissed == false) {
+//     document.write(user.name + '-' + user.salary + '<br>')
+//   }
+// }
+// let students = {
+//   'group1': ['student11', 'student12', 'student13'],
+//   'group2': ['student21', 'student22', 'student23'],
+//   'group3': ['student31', 'student32'],
+// };
+// for(let group in students) {
+//   for (let name of students[group]) {
+//     console.log(name)
+//   }
+// }
+// let data = {
+//   1: [
+//     'data11',
+//     'data12',
+//     'data13',
+//   ],
+//   2: [
+//     'data21',
+//     'data22',
+//     'data23',
+//   ],
+//   3: [
+//     'data31',
+//     'data32',
+//     'data33',
+//     'data34',
+//     'data35',
+//   ],
+//   4: [
+//     'data41',
+//     'data42',
+//   ],
+// }
+// for (let num in data) {
+//   for (let key in data[num]) {
+//     console.log(data[num][key])
+//   }
+// }
+// let data = [
+//   {
+//     1: 'data11',
+//     2: 'data12',
+//     3: 'data13',
+//     4: 'data14',
+//   },
+//   {
+//     1: 'data21',
+//     2: 'data22',
+//     3: 'data33',
+//   },
+//   {
+//     1: 'data31',
+//     2: 'data32',
+//   },
+// ]
+// for (let num in data) {
+//   for (let key in data[num]) {
+//     console.log(key + ': ' + data[num][key])
+//   }
+// }
+// let data = [
+//   {
+//     1: [
+//       'data111',
+//       'data112',
+//       'data113',
+//     ],
+//     2: [
+//       'data121',
+//       'data122',
+//       'data123',
+//     ],
+//   },
+//   {
+//     1: [
+//       'data211',
+//       'data212',
+//       'data213',
+//     ],
+//     2: [
+//       'data221',
+//       'data222',
+//       'data223',
+//       'data224',
+//     ],
+//     3: [
+//       'data231',
+//       'data232',
+//       'data233',
+//       'data234',
+//       'data235',
+//     ],
+//   },
+//   {
+//     1: [
+//       'data411',
+//       'data412',
+//       'data413',
+//     ],
+//     2: [
+//       'data421',
+//     ],
+//   },
+// ]
+// for (let num in data) {
+//   for (let key in data[num]) {
+//     console.log(key + ': ' + data[num][key])
+//   }
+// }
+
+////////////////////////////////////////////////////
 

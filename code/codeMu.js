e27b5b4326fa190f2eba9e251de775395b97c5f8
UglanovEA <uglanovea@gmail.com>
2021-07-21 10:47:06 +0300

    ver 14.0

--- code/codeMu.js
+++ code/codeMu.js
@@ -2404,3 +2404,33 @@
 
 ////////////////////////////////////////////////////
 
+// function getSum(arr) {
+//   let sum = 0;
+//   for (let elem of arr) {
+//     sum += Number(elem);
+//   }
+//   return sum;
+// }
+// function getDigits(num) {
+//   return String(num).split('');
+// }
+// console.log(getSum([1, 2, 3, 4, 5]))
+// console.log(getAvg(getDivisors(24)));
+// // Нахождение среднего арифметического:
+// function getAvg(arr) {
+//   let sum = 0;
+//   for (let elem of arr) {
+//     sum += elem;
+//   }
+//   return sum / arr.length;
+// }
+// // Нахождение массива делителей числа:
+// function getDivisors(num) {
+//   let result = [];
+//   for (let i = 2; i < num; i++) {
+//     if (num % i == 0) {
+//       result.push(i);
+//     }
+//   }
+//   return result;
+// }

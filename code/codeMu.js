1d3f78d8df066dea0e13a16c2a7471cf240f2afe
UglanovEA <uglanovea@gmail.com>
2021-07-22 17:16:57 +0300

    ver 14.2

--- code/codeMu.js
+++ code/codeMu.js
@@ -2464,3 +2464,48 @@
 
 ////////////////////////////////////////////////////
 
+// function isFreindly(num1, num2) {
+//   let sum1 = getSum(getOwnDivisors(num1));
+//   let sum2 = getSum(getOwnDivisors(num2));
+
+//   if (sum1 == num2 && sum2 == num1) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// function getOwnDivisors(num) {
+//   let arr = []
+//   for (let i = 1; i < num; i++) {
+//     if (num % i == 0) {
+//       arr.push(i)
+//     }
+//   } return arr
+// }
+// function getSum(arr) {
+//   let sum = 0
+//   for (let elem of arr) {
+//     sum += elem
+//   } return sum
+// }
+// function getFriendly(num1, num2) {
+//   let arr = [];
+//   for (let i = num1; i <= num2; i++) {
+//     for (let j = num1 + 1; j <= num2; j++) {
+//       if (isFreindly(i, j)) arr.push([i, j]);
+//     }
+//   }
+//   return arr;
+// }
+// console.log(getFriendly(1, 9000));
+// function getSimpleDivisor(num) {
+//   let arr = [];
+//   for (let i = 1; i < num; i++) {
+//     if (num % i == 0) {
+//       arr.push(i);
+//     }
+//   } return arr;
+// }
+// console.log(getSimpleDivisor(220))
+
+////////////////////////////////////////////////////

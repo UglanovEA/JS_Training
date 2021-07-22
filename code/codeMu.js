2dca161c96ff5eb0221c9e20de568038042f6776
UglanovEA <uglanovea@gmail.com>
2021-07-22 17:42:16 +0300

    ver 14.3

--- code/codeMu.js
+++ code/codeMu.js
@@ -2509,3 +2509,42 @@
 // console.log(getSimpleDivisor(220))
 
 ////////////////////////////////////////////////////
+
+// function getIn(arr1, arr2) {
+//   let result = []
+//   for (let elem of arr1) {
+//     if (inArray(elem, arr2)) result.push(elem)
+//   } return result
+// }
+// function inArray(elem, arr) {
+//   if (arr.indexOf(elem) === -1) {
+//     return false;
+//   }
+//   else { return true }
+// }
+// console.log(inArray(3, [1, 2, 3]))
+// console.log(inArray(5, [1, 2, 3]))
+
+////////////////////////////////////////////////////
+
+// let arr1 = [1, 2, 3]
+// let arr2 = [2, 3, 4, 5]
+// let result = []
+// function inArray(elem, arr) {
+//   return arr.indexOf(elem) !== -1;
+// }
+// function getFirstDiff(arr1, arr2) {
+//   let result = []
+//   for (let elem of arr1) {
+//     if (!inArray(elem, arr2)) {
+//       result.push(elem)
+//     }
+//   } return result
+// }
+// function getDiff(arr1, arr2) {
+//   let diff1 = getFirstDiff(arr1, arr2)
+//   let diff2 = getFirstDiff(arr2, arr1)
+//   return [].concat(diff1, diff2)
+// }
+// console.log(getDiff(arr1, arr2))
+

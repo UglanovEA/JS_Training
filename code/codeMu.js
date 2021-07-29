07e04476cd637d59df124932ccd19ad8c33be8bd
UglanovEA <uglanovea@gmail.com>
2021-07-29 16:58:42 +0300

    ver 14.7

--- code/codeMu.js
+++ code/codeMu.js
@@ -2835,3 +2835,58 @@
 // }
 
 // console.log(rangeRand(1, 15));
+
+////////////////////////////////////////////////////
+
+// for (let i = 100000; i <= 999999; i++) {
+//   if (isLucky(i)) {
+//     console.log(i);
+//   }
+// }
+
+// function isLucky(num) {
+//   let str = String(num);
+
+//   let sum1 = Number(str[0]) + Number(str[1]) + Number(str[2]);
+//   let sum2 = Number(str[3]) + Number(str[4]) + Number(str[5]);
+
+//   return sum1 == sum2;
+// }
+
+// console.log(getLuckyTickets());
+
+// function getLuckyTickets() {
+//   let result = [];
+
+//   for (let i = 1001; i <= 999999; i++) {
+//     if (isLucky(i)) {
+//       result.push(i);
+//     }
+//   }
+
+//   return result;
+// }
+
+// function isLucky(num) {
+//   let str = normalizeNum(num);
+
+//   let sum1 = Number(str[0]) + Number(str[1]) + Number(str[2]);
+//   let sum2 = Number(str[3]) + Number(str[4]) + Number(str[5]);
+
+//   return sum1 == sum2;
+// }
+
+// function normalizeNum(num) {
+//   let str = String(num);
+
+//   if (str.length == 5) {
+//     str = '0' + str;
+//   }
+//   if (str.length == 4) {
+//     str = '00' + str;
+//   }
+
+//   return str;
+// }
+
+////////////////////////////////////////////////////

3a70d3283dbec5ca7db38f9d081f8f3a22325927
UglanovEA <uglanovea@gmail.com>
2021-08-23 16:45:15 +0300

    ver 16.8

--- code/codeMu.js
+++ code/codeMu.js
@@ -4065,3 +4065,41 @@
 
 ////////////////////////////////////////////////////
 
+// let result = every([1, 2, 3, 4, 5], function (elem) {
+//   if (elem > 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+
+
+// let result = every([1, 2, 3, 4, 5],
+//   (elem) => elem > 0)
+
+// let result = every([1, 2, 3, 4, 5], function (elem, index) {
+//   if (elem * index > 10) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+
+// let result = every([1, 2, 3, 4, 5],
+//   (elem, index) => elem * index > 10)
+
+// let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+// let result = each(arr, function (elem, index) {
+//   if (elem * index > 10) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+
+// let result = each(arr,
+//   (elem, index) => elem * index > 10)
+
+////////////////////////////////////////////////////
+

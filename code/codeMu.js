2e36a43eb21bd0661d968fdff3928940fb17f66c
UglanovEA <uglanovea@gmail.com>
2021-08-30 17:10:18 +0300

    ver 17.7

--- code/codeMu.js
+++ code/codeMu.js
@@ -4534,3 +4534,35 @@
 
 ////////////////////////////////////////////////////
 
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+
+// let arr = ['a', ...arr1, 'b', 'c', ...arr2];
+// console.log(arr);
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+
+// let arr = ['a', ...arr1, ...arr1, 'b', 'c'];
+// console.log(arr);
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [...arr1, 4, 5, 6];
+
+// let arr = ['a', 'b', 'c', ...arr2];
+// console.log(arr);
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [...arr1, 4, 5, 6];
+// let arr3 = [...arr2, 7, 8, 9];
+
+// let arr = [0, ...arr3];
+// console.log(arr);
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [...arr1];
+
+// console.log(arr2);
+
+////////////////////////////////////////////////////
+

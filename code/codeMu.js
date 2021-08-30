123b75f607d117e3e8a01c4bba504612d38a9baa
UglanovEA <uglanovea@gmail.com>
2021-08-30 16:56:17 +0300

    ver 17.4

--- code/codeMu.js
+++ code/codeMu.js
@@ -4484,3 +4484,19 @@
 
 ////////////////////////////////////////////////////
 
+// let arr = [1, 2, 3, 4, 5];
+// let result = arr.every(elem => elem > 0);
+// console.log(result);
+
+// let arr = [2, 4, 6, 8];
+
+// let result = arr.every(elem => elem % 2 == 0);
+// console.log(result);
+
+// let arr = [1, 2, 3, 4, 5]
+
+// let result = arr.every((elem, index) => (elem * index) < 30)
+// console.log(result)
+
+////////////////////////////////////////////////////
+

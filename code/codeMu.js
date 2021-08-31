6fd87a40afdfa27f22b04050479f404f9ab6075b
UglanovEA <uglanovea@gmail.com>
2021-08-31 15:00:21 +0300

    ver 18.0

--- code/codeMu.js
+++ code/codeMu.js
@@ -4630,3 +4630,29 @@
 
 ////////////////////////////////////////////////////
 
+// function getInt(...arrs) {
+//   let result = []
+//   let arr0 = arrs.shift()
+//   for (let elem of arr0) {
+//     if (inArrays(elem, arrs)) {
+//       result.push(elem)
+//     }
+//   } return result
+// }
+
+// function inArrays(elem, arrs) {
+//   for (let arr of arrs) {
+//     if (!inArray(elem, arr)) return false
+//   } return true
+// }
+
+// function inArray(elem, arr) {
+//   return arr.indexOf(elem) !== -1;
+// }
+
+// // Проверим работу:
+// let result = getInt([1, 2, 3], [2, 3, 4], [4, 3, 2]);
+// console.log(result); // выведет [2, 3]
+
+////////////////////////////////////////////////////
+

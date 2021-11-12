8635a2ca07570d2a61bf9c7e9518514fb4df20bb
UglanovEA <uglanovea@gmail.com>
2021-11-12 16:41:34 +0300

    ver 22.0

--- code/codeMuDOM.js
+++ code/codeMuDOM.js
@@ -16,3 +16,105 @@
 
 // let text = document.querySelector('.block p');
 // console.log(text);
+
+// let button1 = document.querySelector('#button1');
+
+// button1.addEventListener('mouseout', function () {
+//   alert('hey');
+// });
+
+// let button1 = document.querySelector('#button1');
+// let button2 = document.querySelector('#button2');
+
+// button1.addEventListener('click', func1);
+// button2.addEventListener('click', func2);
+// function func1() {
+//   alert(1);
+// }
+
+// function func2() {
+//   alert(2);
+// }
+
+// let text1 = document.querySelector('#elem1');
+// let text2 = document.querySelector('#elem2');
+// let text3 = document.querySelector('#elem3');
+// let text4 = document.querySelector('#elem4');
+// let text5 = document.querySelector('#elem5');
+
+// text1.addEventListener('mouseover', func);
+// text2.addEventListener('mouseover', func);
+// text3.addEventListener('mouseover', func);
+// text4.addEventListener('mouseover', func);
+// text5.addEventListener('mouseover', func);
+// function func() {
+//   alert('message');
+// }
+
+// let button = document.querySelector('#button');
+
+// button.addEventListener('click', func1);
+// button.addEventListener('click', func2);
+
+// function func1() {
+//   alert('1');
+// }
+
+// function func2() {
+//   alert('2');
+// }
+
+// let text1 = document.querySelector('#elem1');
+// text1.addEventListener('click', func1);
+// text1.addEventListener('click', func2);
+// text1.addEventListener('click', func3);
+
+// function func1() {
+//   alert('1');
+// }
+
+// function func2() {
+//   alert('2');
+// }
+
+// function func3() {
+//   alert('3');
+// }
+
+// let text1 = document.querySelector('#elem1');
+
+// text1.addEventListener('click', function () {
+//   alert('1');
+// });
+// text1.addEventListener('click', function () {
+//   alert('2');
+// });
+// text1.addEventListener('click', function () {
+//   alert('3');
+// });
+
+// let button = document.querySelector('#button');
+// let elem1 = document.querySelector('#elem1');
+// let elem2 = document.querySelector('#elem2');
+// button.addEventListener('click', function () {
+//   alert(Number(elem1.innerHTML) + Number(elem2.innerHTML));
+// });
+
+// let button1 = document.querySelector('#button1');
+// let button2 = document.querySelector('#button2');
+// let elem1 = document.querySelector('#elem1');
+// let elem2 = document.querySelector('#elem2');
+// button1.addEventListener('click', function () {
+//   elem1.innerHTML = 'привет';
+//   alert(elem1.innerHTML);
+// });
+// button2.addEventListener('click', function () {
+//   elem2.innerHTML = 'пока';
+//   alert(elem2.innerHTML);
+// });
+
+// let button1 = document.querySelector('#button1');
+// let elem1 = document.querySelector('#elem1');
+// button1.addEventListener('click', function () {
+//   elem1.innerHTML = '<b>жирный текст</b>';
+// });

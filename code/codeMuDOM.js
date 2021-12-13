8fc412ffdabfc065f90cb39de7aad735ced414b9
UglanovEA <uglanovea@gmail.com>
2021-12-13 16:36:16 +0300

    ver 23.9

--- code/codeMuDOM.js
+++ code/codeMuDOM.js
@@ -1039,3 +1039,24 @@
 // });
 
 // ======================================================================188
+
+// let elem = document.querySelector('#elem');
+// let button = document.querySelector('#button');
+// button.addEventListener('click', function () {
+//   elem.focus();
+// });
+
+// let elem1 = document.querySelector('#elem1');
+// let elem2 = document.querySelector('#elem2');
+// elem1.addEventListener('input', function () {
+//   if (elem1.value.length >= 2) {
+//     elem2.focus();
+//   }
+// });
+// elem2.addEventListener('input', function () {
+//   if (elem2.value.length >= 2) {
+//     elem2.blur();
+//   }
+// });
+
+// ======================================================================189

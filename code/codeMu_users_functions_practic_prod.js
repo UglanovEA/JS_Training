49dd0b4c12683692cd394758f354e8ea87397016
UglanovEA <uglanovea@gmail.com>
2022-05-06 10:58:49 +0300

    ver 25.5

--- code/codeMu_users_functions_practic_prod.js
+++ code/codeMu_users_functions_practic_prod.js
@@ -107,3 +107,164 @@
 // alert(num);
 
 // ======================================================================130
+
+// function func(localNum) {
+//   alert(localNum);
+// }
+// func(1);
+
+// function func(localNum) {
+//   alert(localNum);
+// }
+// let num = 1;
+// func(num);
+
+// function func(localNum) {
+//   alert(localNum);
+// }
+// let num = 1;
+// func(num);
+// num = 2;
+
+// let num = 1;
+// function func(localNum) {
+//   alert(localNum);
+// }
+// num = 2;
+// func(num);
+
+// function func(localNum) {
+//   localNum = 2;
+// }
+// let num = 1;
+// func(num);
+// alert(num);
+
+// function func(localNum) {
+//   num = 2;
+// }
+// let num = 1;
+// func(num);
+// alert(num);
+
+// function func(localNum) {
+//   let num = 2;
+// }
+// let num = 1;
+// func(num);
+// alert(num);
+
+// function func(num) {
+//   num = 2;
+// }
+// let num = 1;
+// func(num);
+// alert(num);
+
+// function func() {
+//   num = 2;
+// }
+// let num = 1;
+// func();
+// alert(num);
+
+// function func() {
+//   let num = 2;
+// }
+// let num = 1;
+// func();
+// alert(num);
+
+// ======================================================================131
+
+// function func() {
+//   return '!';
+// }
+// console.log(func);
+
+// ======================================================================132
+
+// function func() {
+//   return 'str';
+// }
+// func = 123;
+// console.log(func);
+
+// function func1() {
+//   return 3;
+// }
+// let func2 = func1;
+// console.log(func1() + func2());
+
+// let func1 = function () {
+//   return 1;
+// };
+// let func2 = function () {
+//   return 2;
+// };
+// console.log(func1() + func2());
+
+// ======================================================================133
+
+// console.log(func());
+// function func() {
+//   return 1;
+// }
+
+// console.log(func1);
+// let func1 = function () {
+//   return 2;
+// };
+
+// let func1 = function () {
+//   alert('!');
+// };
+// let func2 = function () {
+//   alert('!');
+// };
+// function func3() {
+//   alert('!');
+// }
+
+// ======================================================================134
+
+// -function func() {
+//   alert('!');
+// };
+// -1;
+// function func() {
+//   alert('!');
+// }
+// -1;
+// function func() {
+//   alert('!');
+// }
+// 1;
+// function func() {
+//   alert('!');
+// }
+// function func() {
+//   // это Function Declaration
+//   alert('!');
+// }
+// +1;
+// func();
+// function func() {
+//   alert('!');
+// }
+// +1;
+// function func() {
+//   alert('!');
+// }
+// +1;
+// +1;
+// function func() {
+//   alert('!');
+// }
+// +1;
+// function func() {
+//   alert('!');
+// }
+// +alert('!');
+
+// ======================================================================135

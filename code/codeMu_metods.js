605058747e172ea98b32dbeb3b9ea7c2f22863b2
UglanovEA <uglanovea@gmail.com>
2022-05-16 17:46:06 +0300

    ver 26.0

---
+++ code/codeMu_metods.js
@@ -0,0 +1,32 @@
+'use strict';
+// ======================================================================150
+
+// let arr = [1, 2, 3, 4];
+// let result = arr.map((e) => e * 2);
+// console.log(result);
+
+// let arr = ['abc', 'bcd', 'cde'];
+// let result = arr.map((e) => e + '!');
+// console.log(result);
+
+// let arr = ['abc', 'bcd', 'cde'];
+// let result = arr.map((e) => e.split('').reverse().join(''));
+// console.log(result);
+
+// let arr = ['123', '456', '789'];
+// let result = arr.map((e) => e.split(''));
+// console.log(result);
+
+// let arr = [1, 2, 3, 4];
+// let result = arr.map((e, i) => e * i);
+// console.log(result);
+
+// let arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// let result = arr.map((e) => e.map((num) => num * num));
+// console.log(result);
+
+// ======================================================================151

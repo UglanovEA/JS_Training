8b180190e208f61e6e1bae84d4c47af30fcf693c
UglanovEA <uglanovea@gmail.com>
2022-05-26 14:43:32 +0300

    ver 27.6

---
+++ code/JavaScriptТестыДляПроверкиЗнаний.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// function newArr(val) {
+//   return [val];
+// }
+// console.log(newArr(3));
+
+// function func() {
+//   let a = 5;
+//   if (a === 5) {
+//     let b = 10;
+//   }
+//   return b;
+// }
+// console.log(func()); //error
+
+// const arr = [0, 1, 2];
+// console.log(arr.join()); //'1,2,3'
+
+/*==========29 октября =========*/
